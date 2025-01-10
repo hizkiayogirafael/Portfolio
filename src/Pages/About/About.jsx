40c7ad2c56321998c45bca571a0f8b05a10497db
hizkiayogirafael <rafaelyogi2093@gmail.com>
2025-01-11 01:06:24 +0700

    updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -26,18 +26,18 @@ const About = () => {
           <h1 className="text-center lg:text-left font-semibold hidden lg:block lg:text-[36px]">
             btw, Who is..
           </h1>
-          <h1 className="text-center lg:text-left text-[36px] lg:text-[80px] font-bold">
+          <h1 className="text-center lg:text-left text-[36px] xl:text-[80px] font-bold 2xl:text-[58px]">
             hizkia yogi rafael"
           </h1>
-          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row">
+          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row 2xl:text-[25px]">
             - Graduate Informatic Student Study Program of Informatic
             Engineering at Batam State Polytechnic.
           </h1>
-          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row">
+          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row 2xl:text-[25px]">
             - Excellent communication, collaboration, and responsibility at
             work.
           </h1>
-          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row">
+          <h1 className="text-justify font-medium lg:font-normal text-[11px] lg:text-[30px] flex flex-row 2xl:text-[25px]">
             - Specializing in Web Development & Digital Creative .
           </h1>
           <a
